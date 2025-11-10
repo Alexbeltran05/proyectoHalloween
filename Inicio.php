2a2ed8977f273a9aa56552f7f2f8a5b259d0137e
Alex Beltran <abm0032@alu.medac.es>
2025-11-10 20:05:32 +0100

    para lola

--- Inicio.php
+++ Inicio.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <link rel="stylesheet" href="styleInicio.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h2 style='color:orange; text-align:center;'> enhorabuena has sido seleccionado, bienvenido a tu nuevo trabajo </h2>
-    <p style='text-align:center;'>eres el vigilante de seguridad de una pizzeria abandonada, tu turno </p>
-    
+    <div class="card">
+        <h2 style='color:orange; text-align:center;'> enhorabuena has sido seleccionado, bienvenido a tu nuevo trabajo </h2>
+        <p style='text-align:center;'>eres el vigilante de seguridad de una pizzeria abandonada, tu turno termina a las 6 am, Buena suerte. </p>
+    </div>
 </body>
 </html>
